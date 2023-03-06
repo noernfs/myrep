8e225b1b2a88726c4a90ff37b9e688fe9b684984
Muhammad Aji <noer.nfs@gmail.com>
2023-03-07 00:53:00 +0700

    design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,8 +13,8 @@ export default function Footer() {
 <div className=" bg-gray-900">
     <div className="max-w-2xl mx-auto text-white">
         <div className="text-center pt-10">
-            <h3 className="text-3xl mb-3 text-gray-400 text-bold">Revolusi internet <span className="text-blue-900">Bnetfit</span></h3>
-            <p> <span className="text-gray-200">Pelopor Perubahan Progresif Jaringan Internet</span></p>
+            <h3 className="text-3xl mb-3 text-gray-400 font-bold">Revolusi internet <span className="text-blue-900">Bnetfit</span></h3>
+            <p> <span className="text-gray-100">Pelopor Perubahan Progresif Jaringan Internet</span></p>
         </div>
         <div className="mt-10 pb-5 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
             &copy; 2023 Bnetfit Corp. All rights reserved.
--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -14,7 +14,7 @@ export default function Pricing() {
     
     <div className="w-full flex-1 mt-8 p-8 order-2 bg-white shadow-xl rounded-3xl sm:w-96 lg:w-full lg:order-1 lg:rounded-r-none">
       <div className="mb-7 pb-7 flex items-center border-b border-gray-300">
-        <img src="https://res.cloudinary.com/williamsondesign/abstract-1.jpg"  alt="Serbu 10" className="rounded-3xl w-20 h-20" />
+        <img src="new3.webp"  alt="Serbu 10" className="rounded-3xl w-20 h-20" />
         <div className="ml-5">
           <span className="block text-2xl font-semibold">Serbu 20</span>
           <span><span className="font-medium text-gray-500 text-xl align-top">Rp&thinsp;</span><span className="text-3xl font-bold">290rb </span></span><span className="text-gray-500 font-medium">/ bulan</span>
@@ -42,7 +42,7 @@ export default function Pricing() {
     
     <div className="w-full flex-1 p-8 order-1 shadow-xl rounded-3xl bg-gray-900 text-gray-400 sm:w-96 lg:w-full lg:order-2 lg:mt-0">
       <div className="mb-8 pb-8 flex items-center border-b border-gray-600">
-        <img src="https://res.cloudinary.com/williamsondesign/abstract-2.jpg"  alt="Serbu 10" className="rounded-3xl w-20 h-20" />
+        <img src="new1.webp"  alt="Serbu 10" className="rounded-3xl w-20 h-20" />
         <div className="ml-5">
           <span className="block text-3xl font-semibold text-white">Serbu 10</span>
           <span><span className="font-medium text-xl align-top">Rp&thinsp;</span><span className="text-3xl font-bold text-white">199rb </span></span><span className="font-medium">/ bulan</span>
@@ -70,7 +70,7 @@ export default function Pricing() {
     
     <div className="w-full flex-1 mt-8 p-8 order-3 bg-white shadow-xl rounded-3xl sm:w-96 lg:w-full lg:order-3 lg:rounded-l-none">
       <div className="mb-7 pb-7 flex items-center border-b border-gray-300">
-        <img src="https://res.cloudinary.com/williamsondesign/abstract-3.jpg"  alt="Serbu 30" className="rounded-3xl w-20 h-20" />
+        <img src="new2.webp"  alt="Serbu 30" className="rounded-3xl w-20 h-20" />
         <div className="ml-5">
           <span className="block text-2xl font-semibold">Serbu 30 Mbps</span>
           <span><span className="font-medium text-gray-500 text-xl align-top">Rp&thinsp;</span><span className="text-3xl font-bold">440rb </span></span><span className="text-gray-500 font-medium">/ bulan</span>
